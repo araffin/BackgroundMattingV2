2472c9378db848ddd632513ca40f875ecd95d404
Antonin RAFFIN <antonin.raffin@ensta.org>
2021-02-07 13:18:44 +0100

    Try threading

--- start_fake_webcam.sh
+++ start_fake_webcam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python inference_webcam.py  --model-type mattingbase --model-backbone mobilenetv2 --model-checkpoint pytorch_mobilenetv2.pth --resolution 180 180 --num-threads 2 --background green --fake-cam --hide-fps
+python inference_webcam.py --model-type mattingbase --model-backbone mobilenetv2 --model-checkpoint pytorch_mobilenetv2.pth --resolution 252 168 --num-threads 2 --background green --fake-cam --hide-fps
