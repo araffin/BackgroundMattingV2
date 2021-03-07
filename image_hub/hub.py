59e5ec43e027d19f61b92c45b1466247c3554983
Antonin Raffin <antonin.raffin@ensta.org>
2021-03-07 18:47:27 +0100

    Add jpg example

--- image_hub/hub.py
+++ image_hub/hub.py
@@ -1,13 +1,16 @@
 import cv2
 import imagezmq
+import simplejpeg
 
 # Instantiate and provide the first publisher address
 image_hub = imagezmq.ImageHub(open_port="tcp://192.168.12.29:5555", REQ_REP=False)
 # image_hub.connect('tcp://192.168.86.38:5555')    # second publisher address
 
 while True:  # show received images
-    sender_name, image = image_hub.recv_image()
-    cv2.imshow(sender_name, image)  # 1 window for each unique RPi name
+    # sender_name, image = image_hub.recv_image()
+    sender_name, jpg_buffer = image_hub.recv_jpg()
+    image = simplejpeg.decode_jpeg(jpg_buffer, colorspace="BGR")
+    cv2.imshow(sender_name, image)
     key = cv2.waitKey(1) & 0xFF
     if key in [113, 27]:
         break
--- image_hub/send.py
+++ image_hub/send.py
@@ -1,6 +1,7 @@
 import argparse
 import socket
 
+import cv2
 import imagezmq
 
 import nanocamera as nano
@@ -22,10 +23,14 @@ sender = imagezmq.ImageSender(connect_to="tcp://*:5555", REQ_REP=False)
 
 sender_name = socket.gethostname()  # send RPi hostname with each image
 width, height = args.resolution
-
+jpeg_quality = 95  # 0 to 100, higher is better quality
 cam = nano.Camera(flip=0, width=width, height=height, fps=30)
 
 while True:  # send images until Ctrl-C
     image = cam.read()
-    sender.send_image(sender_name, image)
-    # The execution loop will continue even if no subscriber is connected
+    # sender.send_image(sender_name, image)
+    ret_code, jpg_buffer = cv2.imencode(
+        ".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
+    )
+    jpg_buffer = jpg_buffer.tobytes()
+    reply = sender.send_jpg(sender_name, jpg_buffer)
